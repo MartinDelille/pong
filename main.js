0e9d80341f8366afe78e8dce328e656beef43031
Martin Delille <martin@delille.org>
2020-01-18 23:28:16 +0000

    Basic project

---
+++ main.js
@@ -0,0 +1,23 @@
+import { createGitgraph } from "@gitgraph/js";
+
+const graphContainer = document.getElementById("gitgraph");
+const gitgraph = createGitgraph(graphContainer);
+
+import { Gitgraph } from "@gitgraph/react";
+
+// Now you can use the component =>
+<Gitgraph>{(gitgraph) => {}}</Gitgraph>;
+
+const master = gitgraph.branch("master");
+master.commit("Init the project");
+master
+  .commit("Add README")
+  .commit("Add tests")
+  .commit("Implement feature");
+master.tag("v1.0");
+const newFeature = gitgraph.branch("new-feature");
+newFeature.commit("Implement an awesome feature");
+master.commit("Hotfix a bug");
+newFeature.commit("Fix tests");
+// Merge `newFeature` into `master`
+master.merge(newFeature, "Release new version");
